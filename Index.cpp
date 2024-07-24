f42b3fbd047fde68f1b00646272e2bd706ee64db
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-07-24 22:38:54 +0530

    Pattern 6.

--- Index.cpp
+++ Index.cpp
@@ -1,24 +1,24 @@
-#include<iostream>
-#include<conio.h>
+#include <iostream>
+#include <conio.h>
 
 using namespace std;
 
 int main()
 {
-    int Row,Column;
-    cout<<"Enter the number of rows: ";
-    cin>>Row;
-    cout<<"Enter the number of columns: ";
-    cin>>Column;
-    //Pattern 1
-    // for(int i=0;i<Row;i++)
-    // {
-    //     for(int j=0;j<Column;j++)
-    //     {
-    //         cout<<"*";
-    //     }
-    //     cout<<endl;
-    // }
+    int Row, Column;
+    cout << "Enter the number of rows: ";
+    cin >> Row;
+    cout << "Enter the number of columns: ";
+    cin >> Column;
+    // Pattern 1
+    //  for(int i=0;i<Row;i++)
+    //  {
+    //      for(int j=0;j<Column;j++)
+    //      {
+    //          cout<<"*";
+    //      }
+    //      cout<<endl;
+    //  }
 
     // //output
     // *****
@@ -27,71 +27,88 @@ int main()
     // *****
     // *****
 
-    //Pattern 2
-    // for(int i=0;i<Row;i++)
+    // Pattern 2
+    //  for(int i=0;i<Row;i++)
+    //  {
+    //      for(int j=0;j<=i;j++)
+    //      {
+    //          cout<<"*";
+    //      }
+    //      cout<<endl;
+    //  }
+
+    // output of 2'nd
+    //  *
+    //  **
+    //  ***
+    //  ****
+    //  *****
+
+    // pattern 3
+    //  for(int i=1;i<=Row;i++)
+    //  {
+    //      for(int j=1;j<=i;j++)
+    //      {
+    //          cout<<j;
+    //      }
+    //      cout<<endl;
+    //  }
+
+    // output 3rd
+    //  1
+    //  12
+    //  123
+    //  1234
+    //  12345
+
+    // pattern 4
+    //  for(int i=1;i<=Row;i++)
+    //  {
+    //      for(int j=1;j<=i;j++)
+    //      {
+    //          cout<<i;
+    //      }
+    //      cout<<endl;
+    //  }
+
+    // output 4th
+    //  1
+    //  22
+    //  333
+    //  4444
+    //  55555
+
+    // pattern 5
+    //  for(int i=Row;i>0;i--)
+    //  {
+    //      for(int j=i;j>0;j--)
+    //      {
+    //          cout<<"*";
+    //      }
+    //      cout<<endl;
+    //  }
+
+    // output 5th
+    //  *****
+    //  ****
+    //  ***
+    //  **
+    //  *
+
+    // pattern 6
+    // for (int i = Row; i > 0; i--)
     // {
-    //     for(int j=0;j<=i;j++)
+    //     for (int j = i; j > 0; j--)
     //     {
-    //         cout<<"*";
+    //         cout << j;
     //     }
-    //     cout<<endl;
+    //     cout << endl;
     // }
 
-    //output of 2'nd
-    // *
-    // **
-    // ***
-    // ****
-    // *****
-
-    //pattern 3
-    // for(int i=1;i<=Row;i++)
-    // {
-    //     for(int j=1;j<=i;j++)
-    //     {
-    //         cout<<j;
-    //     }
-    //     cout<<endl;
-    // }
-
-    //output 3rd
+    //output 6th
+    // 54321
+    // 4321
+    // 321
+    // 21
     // 1
-    // 12
-    // 123
-    // 1234
-    // 12345
-
-    //pattern 4
-    // for(int i=1;i<=Row;i++)
-    // {
-    //     for(int j=1;j<=i;j++)
-    //     {
-    //         cout<<i;
-    //     }
-    //     cout<<endl;
-    // }
-
-    //output 4th
-    // 1
-    // 22
-    // 333
-    // 4444
-    // 55555
-    
-    //pattern 5
-    // for(int i=Row;i>0;i--)
-    // {
-    //     for(int j=i;j>0;j--)
-    //     {
-    //         cout<<"*";
-    //     }
-    //     cout<<endl;
-    // }
-
-    //output 5th
-    // *****
-    // ****
-    // ***
-    // **
-    // *
 }
