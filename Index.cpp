af8a1cdc9bd7cf4bc85911c8ead542f5281a86c6
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-07-24 22:25:50 +0530

    Code For First Pattern.

--- Index.cpp
+++ Index.cpp
@@ -10,14 +10,14 @@ int main()
     cin>>Row;
     cout<<"Enter the number of columns: ";
     cin>>Column;
-    for(int i=0;i<Row;i++)
-    {
-        for(int j=0;j<Column;j++)
-        {
-            cout<<"*";
-        }
-        cout<<endl;
-    }
+    // for(int i=0;i<Row;i++)
+    // {
+    //     for(int j=0;j<Column;j++)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
 
     // //output
     // *****
