e0d8b95e6cf5e1088e585fbe52c01b3eecdde38f
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-07-26 23:25:30 +0530

    Adding Logic Of Difference Bit From Two Taken Numbers.

--- Bit-Manipulation.cpp
+++ Bit-Manipulation.cpp
@@ -54,9 +54,27 @@ int setBitCheck(int n)
     return count;
 }
 
+int checkDiffBits(int n4,int n5)
+{
+    int count=0;
+    int l_digit=0,l_digit1=0;
+    while(n4!=0)
+    {
+        l_digit=n4%10;
+        l_digit1=n5%10;
+        if((l_digit!=l_digit1))
+        {
+            count++;
+        }
+        n4=n4/10;
+        n5=n5/10;
+    }
+    return count;
+}
+
 int main()
 {
-    int n,n2,n3;
+    int n,n2,n3,n4,n5;
     cout<<"Enter Number : ";
     cin>>n;
     cout<<convertToBinary(n)<<endl;
@@ -65,5 +83,8 @@ int main()
     cout<<convertToDecimal(n2)<<endl;
     cout<<"Enter Binary Number : ";
     cin>>n3;
-    cout<<setBitCheck(n3);
+    cout<<"Total 1's in Binary Number : "<<setBitCheck(n3)<<endl;
+    cout<<"Enter Number 1 And Number 2 : ";
+    cin>>n4>>n5;
+    cout<<"Difference Between Bits Are : "<<checkDiffBits(n4,n5);
 }
